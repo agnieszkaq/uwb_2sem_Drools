9e5ca71a1bde73930e520edced610ae987d48147
Aga K. <59511312+agnieszkaq@users.noreply.github.com>
2020-05-06 09:00:49 +0200

    Finish.

--- CarInsurace/src/main/java/com/sample/CarInsurance.java
+++ CarInsurace/src/main/java/com/sample/CarInsurance.java
@@ -15,25 +15,23 @@ import com.sample.model.Policyholder;
 public class CarInsurance {
 
 	public static void main(String[] args) {
-	try {
+		try {
+			Person person1 = new Person(18, "female", false);
+			Car car1 = new Car("Audi", 1997, 50001.0, "LPG", 3.20, 3);
+			DriveInformation driveInfo1 = new DriveInformation(new GregorianCalendar(2020, 5, 4), true);
+			Policyholder policyholder1 = new Policyholder(person1, car1, driveInfo1);
+
+			InsurancePrice insurancePrice1 = new InsurancePrice(0.0);
+
 			KieServices ks = KieServices.Factory.get();
 			KieContainer kContainer = ks.getKieClasspathContainer();
 			KieSession kSession = kContainer.newKieSession("ksession-rules");
 
-			Person person1 = new Person(18, "male", false);
-			Car car1 = new Car("Audi", 1997, 970.0, "LPG", 3.20, 0);
-			DriveInformation driveInfo1 = new DriveInformation(new GregorianCalendar(1952, 5, 4), true);
-			Policyholder policyholder = new Policyholder(person1, car1, driveInfo1);
-
-			
-			InsurancePrice insurancePrice = new InsurancePrice(0.0);
-
-			policyholder.getDriveInfo().getDateInYearsfromToday();
-			kSession.insert(insurancePrice);
-			kSession.insert(policyholder);
+			kSession.insert(insurancePrice1);
+			kSession.insert(policyholder1);
 			kSession.fireAllRules();
 
-			System.out.println(insurancePrice.toString());
+			System.out.println(insurancePrice1.toString());
 
 		} catch (Throwable t) {
 			t.printStackTrace();
